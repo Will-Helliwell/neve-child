774e4516a839f24d323f6c234c99f6fb88860b5e
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-06-12 09:51:28 +0800

    Create test shortcode.

--- neve-child/functions.php
+++ neve-child/functions.php
@@ -7,3 +7,19 @@ function neve_child_theme_enqueue_styles() {
     // Enqueue the child theme's stylesheet with its own text domain
     wp_enqueue_style( 'neve-child-theme-style', get_stylesheet_uri(), array( 'neve-child-theme-parent-style' ), '1.0.0' );
 }
+
+function print_attributes($atts) {
+    // Define default attributes and merge with user-defined attributes
+    $atts = shortcode_atts(
+        array(
+            'attr1' => 'default1',
+            'attr2' => 'default2',
+        ),
+        $atts,
+        'test_short_code'
+    );
+
+    // Output the merged attributes
+    return 'Attribute 1 is ' . esc_attr($atts['attr1']) . ' and Attribute 2 is ' . esc_attr($atts['attr2']);
+}
+add_shortcode('test_short_code', 'print_attributes');
